3aa7163d5c18edf333db1ffdc41cf781de2806cf
emreertugrl <emrertugrl@gmail.com>
2025-06-06 12:14:45 +0300

    public folder defined

---
+++ app/(routes)/(home)/page.tsx
@@ -0,0 +1,12 @@
+import RecentProfuct from "@/components/sections/recent/RecentProfuct";
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <RecentProfuct />
+    </div>
+  );
+};
+
+export default Home;
