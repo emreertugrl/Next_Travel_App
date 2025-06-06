0b959cbf094f795082a5d42c12eff39cdfc86346
emreertugrl <emrertugrl@gmail.com>
2025-06-06 13:06:26 +0300

    hero swipper defined

--- app/(routes)/(home)/page.tsx
+++ app/(routes)/(home)/page.tsx
@@ -1,9 +1,11 @@
 import RecentProfuct from "@/components/sections/recent/RecentProfuct";
 import React from "react";
+import Hero from "./_components/Hero/Hero";
 
 const Home = () => {
   return (
     <div>
+      <Hero />
       <RecentProfuct />
     </div>
   );
