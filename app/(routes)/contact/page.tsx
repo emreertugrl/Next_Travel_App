942cb4e319f72d35078fa3bd096ed6198589c249
emreertugrl <emrertugrl@gmail.com>
2025-06-06 15:18:53 +0300

    resend mail defined with recaptcha

--- app/(routes)/contact/page.tsx
+++ app/(routes)/contact/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import ContactForm from "./_components/ContactForm";
 
 const ContactPage = () => {
-  return <div>ContactPage</div>;
+  return (
+    <div>
+      <ContactForm />
+    </div>
+  );
 };
 
 export default ContactPage;
